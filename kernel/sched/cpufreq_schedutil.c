d0a3c12d525c4a6530c059f48925adc8018fb39f
Greg Kroah-Hartman <gregkh@google.com>
2021-10-06 10:40:17 +0200

    Merge 4.9.285 into android-4.9-q
    Changes in 4.9.285
    	ocfs2: drop acl cache for directories too
    	usb: gadget: r8a66597: fix a loop in set_feature()
    	usb: musb: tusb6010: uninitialized data in tusb_fifo_write_unaligned()
    	cifs: fix incorrect check for null pointer in header_assemble
    	xen/x86: fix PV trap handling on secondary processors
    	usb-storage: Add quirk for ScanLogic SL11R-IDE older than 2.6c
    	USB: serial: cp210x: add ID for GW Instek GDM-834x Digital Multimeter
    	staging: greybus: uart: fix tty use after free
    	USB: serial: mos7840: remove duplicated 0xac24 device ID
    	USB: serial: option: add Telit LN920 compositions
    	USB: serial: option: remove duplicate USB device ID
    	USB: serial: option: add device id for Foxconn T99W265
    	mcb: fix error handling in mcb_alloc_bus()
    	serial: mvebu-uart: fix driver's tx_empty callback
    	net: hso: fix muxed tty registration
    	net/mlx4_en: Don't allow aRFS for encapsulated packets
    	scsi: iscsi: Adjust iface sysfs attr detection
    	blktrace: Fix uaf in blk_trace access after removing by sysfs
    	net: stmmac: allow CSR clock of 300MHz
    	m68k: Double cast io functions to unsigned long
    	compiler.h: Introduce absolute_pointer macro
    	net: i825xx: Use absolute_pointer for memcpy from fixed memory location
    	sparc: avoid stringop-overread errors
    	qnx4: avoid stringop-overread errors
    	parisc: Use absolute_pointer() to define PAGE0
    	arm64: Mark __stack_chk_guard as __ro_after_init
    	alpha: Declare virt_to_phys and virt_to_bus parameter as pointer to volatile
    	net: 6pack: Fix tx timeout and slot time
    	spi: Fix tegra20 build with CONFIG_PM=n
    	arm64: dts: marvell: armada-37xx: Extend PCIe MEM space
    	qnx4: work around gcc false positive warning bug
    	tty: Fix out-of-bound vmalloc access in imageblit
    	cpufreq: schedutil: Use kobject release() method to free sugov_tunables
    	cpufreq: schedutil: Destroy mutex before kobject_put() frees the memory
    	mac80211: fix use-after-free in CCMP/GCMP RX
    	ipvs: check that ip_vs_conn_tab_bits is between 8 and 20
    	mac80211: limit injected vht mcs/nss in ieee80211_parse_tx_radiotap
    	hwmon: (tmp421) fix rounding for negative values
    	e100: fix length calculation in e100_get_regs_len
    	e100: fix buffer overrun in e100_get_regs
    	ipack: ipoctal: fix stack information leak
    	ipack: ipoctal: fix tty registration race
    	ipack: ipoctal: fix tty-registration error handling
    	ipack: ipoctal: fix missing allocation-failure check
    	ipack: ipoctal: fix module reference leak
    	ext4: fix potential infinite loop in ext4_dx_readdir()
    	net: udp: annotate data race around udp_sk(sk)->corkflag
    	EDAC/synopsys: Fix wrong value type assignment for edac_mode
    	ARM: 9077/1: PLT: Move struct plt_entries definition to header
    	ARM: 9078/1: Add warn suppress parameter to arm_gen_branch_link()
    	ARM: 9079/1: ftrace: Add MODULE_PLTS support
    	ARM: 9098/1: ftrace: MODULE_PLT: Fix build problem without DYNAMIC_FTRACE
    	arm64: Extend workaround for erratum 1024718 to all versions of Cortex-A55
    	HID: betop: fix slab-out-of-bounds Write in betop_probe
    	netfilter: ipset: Fix oversized kvmalloc() calls
    	HID: usbhid: free raw_report buffers in usbhid_stop
    	cred: allow get_cred() and put_cred() to be given NULL.
    	Linux 4.9.285
    
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    Change-Id: I55fa39c89ba10e7092db6001c6feee0d792a401f

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -691,9 +691,17 @@ static struct attribute *sugov_attributes[] = {
 	NULL
 };
 
+static void sugov_tunables_free(struct kobject *kobj)
+{
+	struct gov_attr_set *attr_set = container_of(kobj, struct gov_attr_set, kobj);
+
+	kfree(to_sugov_tunables(attr_set));
+}
+
 static struct kobj_type sugov_tunables_ktype = {
 	.default_attrs = sugov_attributes,
 	.sysfs_ops = &governor_sysfs_ops,
+	.release = &sugov_tunables_free,
 };
 
 /********************** cpufreq governor interface *********************/
@@ -900,7 +908,7 @@ out:
 
 fail:
 	policy->governor_data = NULL;
-	sugov_tunables_free(tunables);
+	sugov_clear_global_tunables();
 
 stop_kthread:
 	sugov_kthread_stop(sg_policy);
@@ -926,10 +934,8 @@ static void sugov_exit(struct cpufreq_policy *policy)
 
 	count = gov_attr_set_put(&tunables->attr_set, &sg_policy->tunables_hook);
 	policy->governor_data = NULL;
-	if (!count) {
-		sugov_tunables_save(policy, tunables);
-		sugov_tunables_free(tunables);
-	}
+	if (!count)
+		sugov_clear_global_tunables();
 
 	mutex_unlock(&global_tunables_lock);
 
